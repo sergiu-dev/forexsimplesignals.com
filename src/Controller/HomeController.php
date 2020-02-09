0c16d835b233906a31ed2f085b1933495461b281
sergiu-dev <sergiu.a.bugeac@gmail.com>
2020-02-09 22:47:34 +0200

    minor change

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\SignalRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,13 +10,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController {
 
 	/**
-	 * @Route("/", name = "app_homepage")
+	 * @Route("/", name = "home")
+	 * @param SignalRepository $signalRepository
 	 * @return Response
 	 */
-	public function index() :Response {
+	public function index(SignalRepository $signalRepository) :Response {
+
+		$signals = $signalRepository->findAll();
 
 		return $this->render('home/index.html.twig', [
-
+			'signals' => $signals,
 		]);
 	}
 }
--- src/Migrations/forex.sql
+++ src/Migrations/forex.sql
@@ -11,44 +11,13 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-
--- Dumping database structure for forex_simple_signals
-DROP DATABASE IF EXISTS `forex_simple_signals`;
-CREATE DATABASE IF NOT EXISTS `forex_simple_signals` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `forex_simple_signals`;
-
--- Dumping structure for table forex_simple_signals.migration_versions
-DROP TABLE IF EXISTS `migration_versions`;
-CREATE TABLE IF NOT EXISTS `migration_versions` (
-  `version` varchar(14) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `executed_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
-  PRIMARY KEY (`version`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
 -- Dumping data for table forex_simple_signals.migration_versions: ~1 rows (approximately)
 /*!40000 ALTER TABLE `migration_versions` DISABLE KEYS */;
 INSERT INTO `migration_versions` (`version`, `executed_at`) VALUES
-	('20200209202403', '2020-02-09 20:24:08');
+	('20200209204347', '2020-02-09 20:44:06');
 /*!40000 ALTER TABLE `migration_versions` ENABLE KEYS */;
 
--- Dumping structure for table forex_simple_signals.signal
-DROP TABLE IF EXISTS `signal`;
-CREATE TABLE IF NOT EXISTS `signal` (
-  `signal_id` int(11) NOT NULL,
-  `trade_pair` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `trade_action` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `price` double DEFAULT NULL,
-  `created` datetime DEFAULT NULL,
-  `take_profit` double DEFAULT NULL,
-  `stop_loss` double DEFAULT NULL,
-  `profit` double DEFAULT NULL,
-  `closed` datetime DEFAULT NULL,
-  `closed_price` double DEFAULT NULL,
-  `status` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`signal_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- Dumping data for table forex_simple_signals.signal: ~0 rows (approximately)
+-- Dumping data for table forex_simple_signals.signal: ~10 rows (approximately)
 /*!40000 ALTER TABLE `signal` DISABLE KEYS */;
 INSERT INTO `signal` (`signal_id`, `trade_pair`, `trade_action`, `price`, `created`, `take_profit`, `stop_loss`, `profit`, `closed`, `closed_price`, `status`) VALUES
 	(227897807, 'GBP/USD', 'SELL', 1.2996, '2020-01-13 09:57:38', 1.29657, 1.30658, 10.9, '2020-01-13 19:18:35', 1.29851, 'CLOSED'),
